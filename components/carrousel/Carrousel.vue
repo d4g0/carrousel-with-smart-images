0a910d745e47da0956d800df936e7dc81659c6b1
d4g0 <tocarralero@gmail.com>
2022-02-13 16:55:51 -0500

    [new component] base: Carrousel

---
+++ components/carrousel/Carrousel.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="w-full h-[500px] bg-slate-100">
+    <div class="flex items-center justify-center w-full h-full">
+      <!-- <SmartPicture :src="picturesSrcs[0]" /> -->
+
+      <div id="glide" class="glide h-full relative">
+        <div class="glide__track h-full" data-glide-el="track">
+          <ul class="glide__slides h-full">
+            <li
+              class="glide__slide h-full flex items-center justify-center"
+              v-for="(src, index) in pics"
+              key="index"
+            >
+              <SmartPicture :src="src" />
+            </li>
+          </ul>
+        </div>
+
+        <div
+          data-glide-el="controls"
+          class="glide__arrows absolute top-1/2 left-0 w-full flex items-center justify-between px-5 md:px-8 lg:px-20"
+        >
+          <button
+            class="glide__arrow glide__arrow--left bg-white/80 backdrop-blur-sm p-1 rounded-full"
+            data-glide-dir="<"
+          >
+            <ChevronLeft class="w-4 h-4" />
+          </button>
+          <button
+            class="glide__arrow glide__arrow--right bg-white/80 backdrop-blur-sm p-1 rounded-full"
+            data-glide-dir=">"
+          >
+            <ChevronRight class="w-4 h-4" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Glide from "@glidejs/glide";
+import { onMounted } from "@vue/composition-api";
+import SmartPicture from "~/components/img/SmartPicture.vue";
+import ChevronLeft from "../icons/ChevronLeft.vue";
+import ChevronRight from "../icons/ChevronRight.vue";
+// state
+
+const pics = [
+  "/img/1920x1280/sara-dubler-Koei_7yYtIo-unsplash.jpg",
+  "/img/1920x1280/alexander-kaunas-hEAGekaiJ8g-unsplash.jpg",
+  "/img/1920x1280/alexander-kaunas-TAgGZWz6Qg8-unsplash.jpg",
+  "/img/1920x1280/cory-bjork-D1yT791Nf9A-unsplash.jpg",
+  "/img/1920x1280/visualsofdana-T5pL6ciEn-I-unsplash.jpg",
+];
+
+var glide;
+
+function initGlide() {
+  if (process.client) {
+    glide = new Glide("#glide", {
+      type: "carousel",
+      startAt: 0,
+    }).mount();
+  }
+}
+
+onMounted(initGlide);
+</script>
+
+<style>
+@import url("node_modules/@glidejs/glide/dist/css/glide.core.min.css");
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,8 @@
 <template>
-  <div class="w-full min-h-screen bg-slate-300 px-5 pt-[300px]">
-    <div class="w-[1000px] h-[250px] relative flex">
-      <!-- <SmartPictureLazy
-        @image-load="onImageLoad"
-        alt="smarty image"
-        src="/img/hotel/visualsofdana-T5pL6ciEn-I-unsplash.jpg"
-        imgClasses=""
-      /> -->
+  <div class="w-full min-h-screen bg-slate-100">
+    <div class="w-full">
+
+      <Carrousel />
     </div>
   </div>
 </template>
@@ -16,7 +12,7 @@ import SmartImage from "~/components/img/SmartImage.vue";
 import SmartPicture from "~/components/img/SmartPicture.vue";
 import SmartImageLazy from "~/components/img/SmartImageLazy.vue";
 import SmartPictureLazy from "~/components/img/SmartPictureLazy.vue";
+import Carrousel from "~/components/carrousel/Carrousel.vue";
 
-function onImageLoad() {
-}
+function onImageLoad() {}
 </script>
