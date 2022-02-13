2e6cd0107c201f4848c75338c9daccad3e3e0717
d4g0 <tocarralero@gmail.com>
2022-02-13 17:21:18 -0500

    - autoplay

--- components/carrousel/Carrousel.vue
+++ components/carrousel/Carrousel.vue
@@ -61,7 +61,7 @@ function initGlide() {
     glide = new Glide("#glide", {
       type: "carousel",
       startAt: 0,
-      autoplay: 4000,
+    //   autoplay: 4000,
       hoverpause:true,
       keyboard:true,
     }).mount();
